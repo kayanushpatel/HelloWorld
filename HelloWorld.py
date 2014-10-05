3f47f49d78f8a91ece353803d88333888ba8b1c5
Kayanush Patel <kayanushpatel@gmail.com>
2014-10-04 21:30:19 -0400

    Added HelloWorld.py to repo

---
+++ HelloWorld.py
@@ -0,0 +1,21 @@
+""" Simple hello world program using python for testing
+	github
+
+	Author: Kayanush Patel
+	File: HelloWorld.py
+	Created: 10/04/2014
+"""
+
+def hello_world_function():
+
+	# Prints Hello World!
+
+	print("Hello World!")
+
+def main():
+
+	# Takes hello_world_function and calls it in the main method.
+	
+	hello_world_function()
+
+main()
